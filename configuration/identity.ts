27c7dcb97181d1eef31ffba3078fe0a122f01c44
katie-cooper <122311215+katie-cooper@users.noreply.github.com>
2025-02-25 15:15:23 -0500

    Update identity.ts owner description, ai tone, ai role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Katie`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Undergraduate UNC Student. Major: Computer Science. Minor: Data Science, Studio Art.`;
 
 export const AI_NAME: string = `SmartTax`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Intelligent`;
+export const AI_ROLE: string = `Tax Assistant`;
